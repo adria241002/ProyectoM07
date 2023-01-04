63a8406e7844e788dfbe98b1409d62c96a10d273
manolo99lolo <71652091+manolo99lolo@users.noreply.github.com>
2023-01-04 19:28:41 +0100

    Update esborrarFabricant.php

--- fabricantes/esborrarFabricant.php
+++ fabricantes/esborrarFabricant.php
@@ -1,5 +1,7 @@
 <?php
+    // Generamos la conexion
     include('../connection.php');
+    // Borramos los productos con ese codigo y los fabricantes
     $insert2 = "DELETE FROM producto WHERE codigo_fabricante='" . $_GET['p'] ."'";
     $insert = "DELETE FROM fabricante WHERE codigo='" . $_GET['p'] ."'";
     $esCorrecte2 = mysqli_query($conn,$insert2);
@@ -7,5 +9,6 @@
     if ($esCorrecte) echo "Fabricant eliminat correctament";
     if (!$esCorrecte) echo "Fabricant eliminat erroneament";
     mysqli_close($conn);
+    // Lo mandamos otra vez a la pagina de modificacion
     header("Location: ./delete&modify.php");
 ?>
