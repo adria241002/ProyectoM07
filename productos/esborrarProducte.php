99e07bcfcc6f0ddb1c371b263b7b73342f48e82c
manolo99lolo <71652091+manolo99lolo@users.noreply.github.com>
2023-01-04 19:33:20 +0100

    Update esborrarProducte.php

--- productos/esborrarProducte.php
+++ productos/esborrarProducte.php
@@ -1,5 +1,7 @@
 <?php
+//Generamos la conexion
     include('../connection.php');
+// Borramos el producto con ese codigo y lo mandamos a la pagina de modificar o eliminar productos
     $insert = "DELETE FROM producto WHERE codigo='" . $_GET['p'] ."'";
     $esCorrecte = mysqli_query($conn,$insert);
     if ($esCorrecte) echo "Producte eliminat correctament";
