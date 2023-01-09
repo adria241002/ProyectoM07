8c56bdf1de6791a5f41b3f3a55f6e819641e87a4
adria241002 <71822583+adria241002@users.noreply.github.com>
2023-01-09 20:25:54 +0100

    Update productes.php

--- productes.php
+++ productes.php
@@ -122,17 +122,17 @@ $paginas = ceil($cantRegistros / $cantRegiPorPag);
                 <!-- Printamos el paginador -->
                 <nav aria-label="Page navigation example">
                     <ul class="pagination pt-2">
-                        <li class="page-item <?php echo $_GET['pagina'] <= 1 ? 'disabled' : '' ?>"><a class="page-link" href="productes.php?pagina=<?php echo $_GET['pagina'] - 1 ?>"><</a>
+                        <li class="page-item <?php echo $_GET['pagina'] <= 1 ? 'disabled' : '' ?>"><a class="page-link" href="productes.php?pagina=<?php echo $_GET['pagina'] - 1 ?>&codfab=<?php echo $codigo ?>"><</a>
                         </li>
 
                         <!-- Printamos la cantidad de paginas con sus identificadores numericos  -->
                         <?php for ($i = 0; $i < $paginas; $i++) : ?>
                             <li class="page-item <?php echo $_GET['pagina'] == $i + 1 ? 'active' : '' ?>">
-                                <a class="page-link" href="productes.php?pagina=<?php echo $i + 1 ?>"><?php echo $i + 1 ?></a>
+                                <a class="page-link" href="productes.php?pagina=<?php echo $i + 1 ?>&codfab=<?php echo $codigo ?>"><?php echo $i + 1 ?></a>
                             </li>
                         <?php endfor ?>
 
-                        <li class="page-item <?php echo $_GET['pagina'] >= $paginas ? 'disabled' : '' ?>"><a class="page-link" href="productes.php?pagina=<?php echo $_GET['pagina'] + 1 ?>">></a></li>
+                        <li class="page-item <?php echo $_GET['pagina'] >= $paginas ? 'disabled' : '' ?>"><a class="page-link" href="productes.php?pagina=<?php echo $_GET['pagina'] + 1 ?>&codfab=<?php echo $codigo ?>">></a></li>
                     </ul>
                 </nav>
             </div>
